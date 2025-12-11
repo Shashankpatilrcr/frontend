f2c181666da49cf4a2b88cfae24c5a7dd923b209
Shashankpatilrcr <shashankrcr9@gmail.com>
2025-12-11 22:21:21 +0530

    Update script.js

--- forms/script.js
+++ forms/script.js
@@ -1,27 +1,28 @@
 
 
 
- // let score=30;
-        // score=score+5;
-        // const a=document.querySelector("#out").innerHTML="Final score"+score;
+ let score=30;
+        score=score+5;
+        const a=document.querySelector("#out").innerHTML="Final score"+score;
 
-//         function sayHI(){
-//             console.log("hello")
-//         }
-//         sayHI()
+        function sayHI(){
+            console.log("hello")
+        }
+        sayHI()
 
-//     let a=[10,20,30,40,50];
-//     console.log(a)
-//  const user={firstname:'shashank',
-//     lastname:'patil',
-//     age:19}
-//   console.log(user)
+    let a=[10,20,30,40,50];
+    console.log(a)
+ const user={firstname:'shashank',
+    lastname:'patil',
+    age:19}
+  console.log(user)
 
-// let a=5;
-// let b=3;
-// console.log(a+b)
-// console.log(a-b)
-// console.log(a*b)
-// console.log(a/b)
+let a=5;
+let b=3;
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+
 
 
